a752540b57daf26a895ab797042a711e57f9ddc3
SubhanHakim <subhanhakim07@gmail.com>
2025-12-31 14:54:48 +0700

    redeploy

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -14,8 +14,8 @@ const Footer = () => {
 
                 {/* Right: Minimal Links */}
                 <div className="flex items-center gap-8">
-                    <MinimalLink href="https://x.com/Interpoint_dev" label="X / TERMINAL" />
-                    <MinimalLink href="https://pump.fun/coin/BWN7cMhZ2WvHnpYRbEMBwND2py2twpkDtVXWcvFnpump" label="PUMP.FUN" />
+                    <MinimalLink href="#" label="X / TERMINAL" />
+                    <MinimalLink href="#" label="PUMP.FUN" />
                 </div>
             </div>
         </footer>
--- src/features/landing/LandingOverlay.tsx
+++ src/features/landing/LandingOverlay.tsx
@@ -33,8 +33,8 @@ const LandingOverlay = () => {
 
                     {/* Action Buttons */}
                     <div className="flex gap-6 pointer-events-auto">
-                        <SocialButton href="https://x.com/i/communities/2006234565036159007" label="X / TERMINAL" />
-                        <SocialButton href="https://pump.fun/coin/BWN7cMhZ2WvHnpYRbEMBwND2py2twpkDtVXWcvFnpump" label="PUMP.FUN" />
+                        <SocialButton href="#" label="X / TERMINAL" />
+                        <SocialButton href="#" label="PUMP.FUN" />
                     </div>
                 </div>
             </main>
